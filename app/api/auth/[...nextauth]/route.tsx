6d93a7091d6388a9302a5fd41d04c37dd1df349c
leezizon <99871097+leezizon@users.noreply.github.com>
2024-06-17 02:42:57 +0900

    Update route.tsx
    cococo

--- app/api/auth/[...nextauth]/route.tsx
+++ app/api/auth/[...nextauth]/route.tsx
@@ -1,15 +1,5 @@
 import NextAuth from "next-auth";
-import NaverProvider from "next-auth/providers/naver";
-export const authOptions = {
-    // Configure one or more authentication providers
-    providers: [
-        NaverProvider({
-            clientId: "VOIhuplf7Mqbng97X8al" ?? "",
-            clientSecret: "TJQzkN6Q5m" ?? "",
-        }),
-    // ...add more providers here
-    ],
-};
+import { authOptions } from "../../../utils/authOptions";
      
     const handler = NextAuth(authOptions);
      
